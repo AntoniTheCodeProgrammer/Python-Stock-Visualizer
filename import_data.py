f22eefe27ffae63f58b64c7ec790b7ee0b44cd24
Antoni Żmujdzin <antonizmujdzin@MacBook-Air-NIEBIESKI-Tosia-zazdrosci.local>
2025-11-18 16:28:43 +0100

    import_data file

--- import_data.py
+++ import_data.py
@@ -1,8 +1,14 @@
 import yfinance as yf
 
 # 1. Pobieramy dane dla Apple (ticker: AAPL)
-print("Pobieranie danych...")
-data = yf.download("AAPL", start="2023-01-01", end="2023-12-31")
+
+# data = yf.download("AAPL", start="2023-01-01", end="2023-12-31")
 
 # 2. Wyświetlamy pierwsze 5 wierszy, żeby zobaczyć czy coś przyszło
-print(data.head())
+# print(data.head())
+def download_data(campanies):
+    print("Pobieranie danych...")
+    for company in campanies:
+        data = yf.download("AAPL", start="2023-01-01", end="2023-12-31")
+
+    return data
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from import_data import download_data
+
 print("dasdas")
 antos = True
 if antos:
     print("to ja")
 
 i = 1
+
+print(download_data(["AAPL"]))
