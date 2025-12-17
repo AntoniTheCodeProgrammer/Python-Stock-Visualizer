b263237bb0e6e464438a350ab3601a214a440ee6
Antoni Żmujdzin <antonizmujdzin@MacBook-Air-NIEBIESKI-Tosia-zazdrosci.local>
2025-12-17 23:33:04 +0100

    Changes Page

--- import_data.py
+++ import_data.py
@@ -1,9 +1,49 @@
-import yfinance as yf
+import yfinance
+import pandas
 
 def download_data(companies, start_date, end_date):
     print("Pobieranie danych...")
     
-    data = yf.download(companies, start_date, end_date)
+    data = yfinance.download(companies, start_date, end_date)
     data.fillna(method = "ffill", inplace=True)
 
     return data
+
+def all_movers(period = "Month"):
+    companies_list = [
+        "AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA", "META", "NFLX",
+        "AMD", "INTC", "IBM", "ORCL", "CRM", "ADBE",
+        "DIS", "NKE", "SBUX", "MCD", "KO", "PEP",
+        "JPM", "BAC", "V", "MA", "PYPL"
+    ]
+
+    converter = {"Day" : "2d", "Week" : "5d", "Month" : "1mo", "Year" : "1y"}
+    updated_period = converter[period]
+
+    data = yfinance.download(
+        companies_list, period=updated_period, group_by='ticker'
+    )
+
+    result = []
+
+    for company in companies_list:
+        try:
+            data_company = data[company]["Close"]
+            
+            start_price = data_company.iloc[0]
+            end_price = data_company.iloc[-1]
+            percent = (end_price - start_price)/start_price*100
+
+            result.append({
+                "Name" : company,
+                "Change" : round(percent, 2),
+                "Start Price" : round(start_price, 2),
+                "End Price" : round(end_price, 2)
+            })
+
+        except:
+            continue
+
+    df_result = pandas.DataFrame(result)
+    # print(df_result.head(5))
+    return df_result
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from import_data import download_data
+from import_data import download_data, all_movers
 import matplotlib.pyplot as plt
 import streamlit
 import altair
@@ -11,7 +11,7 @@ min_date = actual_date.replace(year=actual_date.year - 5)
 
 streamlit.set_page_config(layout="wide")
 
-main, candle_page = streamlit.tabs(["Main", "Candle Graph"])
+main, candle_page, changes_page = streamlit.tabs(["Main", "Candle Graph", "Changes Page"])
 
 
 with main:
@@ -44,24 +44,31 @@ with main:
         for i in range(5-number):
             streamlit.space("large")
 
-    with streamlit.spinner('Pobieranie danych z giełdy...'):
+    with streamlit.spinner(f"Loading data..."):
         companies = companies[:number]
         data = download_data(companies, start_date, end_date)
         close_prices = data["Close"]
 
     # Main chart
-    with right:
-        with streamlit.container(border=True):
-            streamlit.line_chart(close_prices)
-        with streamlit.container(border=True):
-            streamlit.write("Price change:")
-            stock_change = streamlit.columns(number, border=True)
-            for i in range(number):
-                if companies[i] != "":
-                    start_price = data.head(1)["Close"][companies[i]].values[0]
-                    end_price = data.tail(1)["Close"][companies[i]].values[0]
-                    percent = (end_price - start_price)/start_price*100
-                    stock_change[i].metric(label=companies[i], value=f"{round(end_price, 2)}$", delta=f"{round(percent, 2)}%")
+        with right:
+            with streamlit.container(border=True):
+                streamlit.line_chart(close_prices)
+
+            with streamlit.container(border=True):
+                streamlit.write("Price change:")
+
+                borders = 0
+                for i in range(number):
+                    if companies[i] != "":
+                        borders += 1
+
+                stock_change = streamlit.columns(len([comp for comp in companies if comp.strip() != ""]), border=True)
+                for i in range(number):
+                    if companies[i] != "":
+                        start_price = data.head(1)["Close"][companies[i]].values[0]
+                        end_price = data.tail(1)["Close"][companies[i]].values[0]
+                        percent = (end_price - start_price)/start_price*100
+                        stock_change[i].metric(label=companies[i], value=f"{round(end_price, 2)}$", delta=f"{round(percent, 2)}%")
 
 with candle_page:
     left, right = streamlit.columns([1, 3], gap="medium")
@@ -80,7 +87,7 @@ with candle_page:
             format = "DD/MM/YY"
         )
 
-    with right, streamlit.container(border=True):
+    with right, streamlit.container(border=True), streamlit.spinner(f"Loading data..."):
         with streamlit.spinner('Pobieranie danych z giełdy...'):
             data_candle = download_data(candle_company, start_date_candle, end_date_candle)
         
@@ -110,4 +117,44 @@ with candle_page:
 
         streamlit.altair_chart(chart, use_container_width=True)
           
+with changes_page:
+    with streamlit.container(border=True):
+        streamlit.write("### Actual Changes")
+
+        options = ["Day", "Week", "Month", "Year"]
+
+        selection = streamlit.segmented_control(
+            "Period", options, selection_mode="single", default="Month"
+        )
+
+    with streamlit.spinner(f"Loading data..."):    
+        data_changes = all_movers(selection)    
+        data_sorted = data_changes.sort_values(by="Change", ascending=False)
         
+        top_gainers = data_sorted.head(5)
+        top_losers = data_sorted.tail(5).sort_values(by="Change", ascending=True)
+
+        col1, col2 = streamlit.columns(2)
+
+        
+        with col1, streamlit.container(border=True):
+            streamlit.success("Największe Wzrosty")
+            streamlit.dataframe(
+                top_gainers.style.format({"Change (%)": "{:+.2f}%", "Price ($)": "{:.2f}$"}), 
+                use_container_width=True,
+                hide_index=True
+            )
+
+        with col2, streamlit.container(border=True):
+            streamlit.error("Największe Spadki")
+            streamlit.dataframe(
+                top_losers.style.format({"Change (%)": "{:+.2f}%", "Price ($)": "{:.2f}$"}), 
+                use_container_width=True,
+                hide_index=True
+            )
+            
+        with streamlit.container(border=True):
+            streamlit.subheader("Wizualizacja Zmian")
+            streamlit.bar_chart(data_sorted.set_index("Name")['Change'])
+
+          
